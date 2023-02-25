c0a695dbae3dfe8bc8d5737d4fbb52658fabe862
Mohammad Anas <85899270+MohammadAnas5@users.noreply.github.com>
2023-02-25 23:38:51 +0530

    Delete 2-conditional-program directory

--- 2-conditional-program/5_problem.py
+++
@@ -1,8 +0,0 @@
-# Kunal is allowed to go out with his friends only on the even days of a given month. Write a program to count the number of days he can go out in the month of August.
-
-count=0
-for i in range(0,30):
-    if i%2==0:
-        count+=1
-
-print(count)
